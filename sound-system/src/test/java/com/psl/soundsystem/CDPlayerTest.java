bbbb7ea13ce0ad7d3137fd2f07320c0f00571a1a
karanpaldewgun <karanpal_dewgun@persistent.co.in>
2016-07-14 16:42:16 +0530

    2.3 Creating Beans in Configuration Class

--- sound-system/src/test/java/com/psl/soundsystem/CDPlayerTest.java
+++ sound-system/src/test/java/com/psl/soundsystem/CDPlayerTest.java
@@ -12,22 +12,27 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.psl.soundsystem.configurations.CDPlayerConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=CDPlayerConfig.class)
+@ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-	
+
 	@Autowired
 	private MediaPlayer player;
-	
+
 	@Autowired
 	private CompactDisc cd;
-	
+
 	@Test
 	public void cdShouldNotBeNull() {
 		Assert.assertNotNull(cd);
 	}
-	
+
+	@Test
+	public void mediaPlayerNotBeNull() {
+		Assert.assertNotNull(player);
+	}
+
 	@Test
 	public void play() {
 		String expectedMessage = "Playing Sgt. Pepper's Lonely Hearts Club by The Beatles\n";
